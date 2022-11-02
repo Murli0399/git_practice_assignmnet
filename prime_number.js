3ddde5aef8ffcf7f631c6eb9914be1a2fc0e04b7
Murli0399 <murlikhaire28@gmail.com>
2022-11-02 20:10:58 +0530

    The Program of Prime Number

---
+++ prime_number.js
@@ -0,0 +1,14 @@
+let n=29;
+let count=0;
+
+for(let i=1;i<=n;i++){
+  if(i%n==0){
+    count++;
+  }
+}
+if(count==2){
+  console.log(n,"is a prime number");
+}
+else{
+  console.log(n,"is a not prime number");
+}
